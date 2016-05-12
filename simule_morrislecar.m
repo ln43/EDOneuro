ba890fdd88a641f18d15c11afe035b2e74277f27
Hélène VASSAL <helene.vassal@insa-lyon.fr>
2016-05-12 16:31:34 +0200

    Quest 4.

--- simule_morrislecar.m
+++ simule_morrislecar.m
@@ -1,7 +1,7 @@
 function sol=simule_morrislecar()
   % SIMULE_MORRISLECAR modele neurone de Morris-Lecar
 
-%>>> Initialisation des paramètres
+%>>> Initialisation des parametres
 I = 400;      % courant applique (muA/cm2)
 duree = 200;  % duree de l'application du courant
 
@@ -21,26 +21,26 @@ T0 =    15;   % Constante de temps pour ouverture des canaux (ms) (1/lambda dans
 par = [I, duree, g_L, g_Ca, g_K, V_L, ...
         V_Ca, V_K, V1, V2, V3, V4, C, T0];
 
-% paramètres de simulation
+% >>> Parametres de simulation
 t0 = -20;
 tfinal = 200;
 tspan = [t0,tfinal];
 options = odeset();
 
-% Conditions initiales
+% >>> Conditions initiales
 IC = [-35;0];
     
-% >>>Solutions
+% >>> Solutions
 sol = ode23(@morrislecar,tspan,IC,options);
 
-% >>> Sortie Graphique
+% >>> Sortie Graphique 1
 figure(1); clf;
 plot(sol.x,sol.y(1,:))
 xlabel('temps (ms)')
 ylabel('V (mV)')
 axis tight
 
-% Recherche Précision
+% >>> Recherche Precision
 options = odeset('AbsTol',1e-9,'RelTol',1e-6);
 sol_precise = ode23(@morrislecar,tspan,IC,options);
 figure(1);
@@ -49,49 +49,81 @@ plot(sol.x,sol.y(1,:),'r--')
 title('V en fonction de t')
 legend('Abs Tol 1e-3', 'Abs Tol 1e-9')
 
-%plot des nullclines
+% >>> Plot des nullclines
 figure(2);clf;
 plot(sol.y(1,:),sol.y(2,:))
 hold on
 vv = linspace(-60,60);
 Mifty=1/2*(1+tanh((vv-V1)/V2));
 Nifty=1/2*(1+tanh((vv-V3)/V4));
-i=2
-legendinfo{1}=['Trajectoire de (V,N)']
+i=2;
+legendinfo{1}=['Trajectoire de (V,N)'];
 for I=100:100:500
     v_null = (I-g_L.*(vv-V_L)-g_Ca.*Mifty.*(vv-V_Ca))./(g_K.*(vv-V_K));
     solV = ode23(@morrislecar,[0,200],IC,options);
     figure(2);
     plot(vv,v_null,'--')
-    legendinfo{i}=['Isocline Nulle V, I=' num2str(I)]
+    legendinfo{i}=['Isocline Nulle V, I=' num2str(I)];
     hold on
 
     figure(3);
     plot(solV.x,solV.y(1,:))
-    legendinfo2{i-1}=['I=' num2str(I)]
+    legendinfo2{i-1}=['I=' num2str(I)];
     hold on
-    i=i+1
+    i=i+1;
 end
 
-figure(2)
+figure(2);
 n_null = Nifty;
 plot(vv,n_null,'--')
-ylim([0,1.4])
-legendinfo{7}=['Isocline nulle de N']
-legend(legendinfo)
+ylim([0,1.4]);
+legendinfo{7}=['Isocline nulle de N'];
+legend(legendinfo);
 title('Portrait de phase')
 xlabel('V(t)')
 ylabel('N(t)')
 
-figure(3)
-legend(legendinfo2)
+figure(3);
+legend(legendinfo2);
 xlabel('temps (ms)')
 ylabel('V (mV)')
 title('V en fonction de t')
 axis tight
 
+% >>> Diagramme de bifurcation
+figure(4); clf;
+index=1;
+for I=100:5:500 
+    sol = ode23(@morrislecar,[0,500],IC,options);
+    L=length(sol.y(1,:));
+    maxS(index)=max(sol.y(1,int64(7*L/8):L));%Suppression de la solution transitoire en prenant la solution au bout d'un certain temps
+    minS(index)=min(sol.y(1,int64(7*L/8):L));
+    index=index+1;
+end
 
-% Fonctions imbriquées
+plot(100:5:500,maxS,'k',100:5:500,minS,'k');
+xlabel('parametre de bifurcation I (muA/cm2)')
+ylabel('potentiel V (mV)')
+
+eq_stst = @(y) sum(morrislecar(0,y).^2);
+index=1;
+y0=[-16,0.13];
+y_stst=ones(2,81);
+for I=100:5:500 
+    if index==1
+        y_stst(:,index)=fminsearch(eq_stst,y0);
+    else
+        y_stst(:,index)=fminsearch(eq_stst,y_stst(:,index-1));
+    end
+    index=index+1;
+end
+figure(4); 
+hold on
+plot(100:5:500,y_stst(1,:),'k--') % plot le premier �l�ment de y_stst en traits discontinus
+title('Diagramme de bifurcation')
+
+
+% >>> Fonctions imbriquees
     function dydt = morrislecar(t,y)
         % MORRISLECAR equations du modele neurone de Morris-Lecar
         Minf=1/2*(1+tanh((y(1)-V1)/V2));
